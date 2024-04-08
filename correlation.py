d0e132be7c1695038dd2b61db62e65e3b6ebc990
Ahnaf Tajwar <tajwara@bu.edu>
2024-04-08 02:15:55 -0400

    initial commit

---
+++ correlation.py
@@ -0,0 +1,13 @@
+"""
+Ahnaf Tajwar
+Class: CS 677
+Date: 4/07/24
+Homework Problem # 1
+Description of Problem (just a 1-2 line summary!): This problem is to
+"""
+
+import pandas as pd
+
+df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+
+print(df)
