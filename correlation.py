a720984dbe93d4b376b2234d628b562459cdc1a6
Ahnaf Tajwar <tajwara@bu.edu>
2024-04-08 02:53:41 -0400

    created two dataframes for each deathevent

--- correlation.py
+++ correlation.py
@@ -10,4 +10,16 @@ import pandas as pd
 
 df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 
-print(df)
+print(df.head())
+
+# Filtering dataframe for death event = 0 and the four specific features
+df_0 = df[df['DEATH_EVENT'] == 0][['creatinine_phosphokinase', 'serum_creatinine', 'serum_sodium', 'platelets']]
+
+# Filtering dataframe for death event = 1 and the four specific features
+df_1 = df[df['DEATH_EVENT'] == 1][['creatinine_phosphokinase', 'serum_creatinine', 'serum_sodium', 'platelets']]
+
+print("DataFrame for death event = 0:")
+print(df_0.head())
+
+print("\nDataFrame for death event = 1:")
+print(df_1.head())
